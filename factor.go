989906454de0da5070cb9b1aa8c1b63084076a6d
Caleb Smith <karagenit@outlook.com>
2017-07-10 04:49:18 +0000

    Find Largest Palindrome

--- factor.go
+++ factor.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 import "os"
 import "math"
+import "strconv"
 
-func isPalin(i uint) bool {
-	return false
+func isPalin(num int) bool {
+	str := strconv.Itoa(num)
+	len := len(str)
+
+	for i := 0; i < len; i++ {
+		if str[i] != str[len-i-1] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
@@ -17,13 +27,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	maxfactor := uint(math.Pow(10, float64(maxlen))) - 1
+	maxfactor := int(math.Pow(10, float64(maxlen))) - 1
+	maxpalin := 0
 
 	for a := maxfactor; a > 0; a-- {
-		for b := a; b > 0; b-- {
-			if isPalin(a * b) {
-				fmt.Println(a * b)
-				os.Exit(0)
+		for b := maxfactor; b > 0; b-- {
+			if (a*b) > maxpalin && isPalin(a*b) { //which order is best?
+				maxpalin = a * b
 			}
 			//test
 			fmt.Println(a * b)
@@ -31,4 +41,7 @@ func main() {
 		//test
 		fmt.Println("---")
 	}
+
+	fmt.Println("Ye")
+	fmt.Println(maxpalin)
 }
