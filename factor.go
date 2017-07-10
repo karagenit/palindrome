8276f3d47fe6703f9428fab6d88644b0d77ee961
Caleb Smith <karagenit@outlook.com>
2017-07-10 04:28:40 +0000

    Read Input, Find Max Factor

--- factor.go
+++ factor.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "os"
+import "math"
 
 func main() {
-	fmt.Println("Hi")
+	var maxlen int
+	_, err := fmt.Scanf("%d", &maxlen)
+
+	if err != nil {
+		fmt.Println("Bad Input!")
+		os.Exit(1)
+	}
+
+	maxfactor := uint(math.Pow(10, float64(maxlen))) - 1
+
+	fmt.Println(maxfactor)
 }
